7941ef402fe3a94e29ccf916f23573d8421c7e80
Anton Kalinin <akalinin@solovat.net>
2016-03-09 12:39:35 +0300

    Add notifications.h

---
+++ notifications.h
@@ -0,0 +1,35 @@
+/****************************************************************************
+ *                                                                           *
+ *  Copyright (C) 2014-2015 iBuildApp, Inc. ( http://ibuildapp.com )         *
+ *                                                                           *
+ *  This file is part of iBuildApp.                                          *
+ *                                                                           *
+ *  This Source Code Form is subject to the terms of the iBuildApp License.  *
+ *  You can obtain one at http://ibuildapp.com/license/                      *
+ *                                                                           *
+ ****************************************************************************/
+
+#ifndef notifications_h
+#define notifications_h
+
+#define kAPP_NOTIFICATION_WIDGET_TAP           @"widgetTapNotification"
+#define kAPP_NOTIFICATION_TABBAR_ITEM_TAP      @"tabBarItemTapNotification"
+#define kAPP_NOTIFICATION_SIDEBAR_ITEM_TAP     @"sideBarItemTapNotification"
+#define kAPP_NOTIFICATION_SEARCH_ITEM_TAP      @"searchItemTapNotification"
+#define kAPP_NOTIFICATION_SEARCH_VARIANTS_TAP  @"searchVariantsItemTapNotification"
+#define kAPP_NOTIFICATION_LANGUAGE_CHANGE      @"changeLanguageNotification"
+
+#define kAPP_NOTIFICATION_TOGGLE_SWIPE         @"navBarToggleSwipeTap"
+
+#define kAPP_NOTIFICATION_FULLSCREEN_ITEM_TAP  @"navBarFullscreenItemTap"
+#define kAPP_NOTIFICATION_MENU_ITEM_TAP        @"navBarMenuItemTap"
+
+#define kAPP_NOTIFICATION_HOME_ITEM_TAP        @"navBarHomeItemTap"
+#define kAPP_NOTIFICATION_BACK_ITEM_TAP        @"navBarBackItemTap"
+
+#define kAPP_NOTIFICATION_RELEASE_OBJECT       @"releaseObjectNotification"
+
+#define kAPP_NOTIFICATION_REMOTE_CONTROL       @"RemoteControlNotificationReceived"
+
+
+#endif
